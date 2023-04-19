10943eba83958c08072d4e6352527b30122af7e7
20230854 <126742656+20230854@users.noreply.github.com>
2023-04-19 15:05:52 +1200

    Delete 2. Sleep calculator.py

--- 2. Sleep calculator.py
+++
@@ -1,9 +0,0 @@
-
-print ("Welcome to Sleep Record Program\n")
-
-user_yob = int(input("Please enter your year of birth...\n"))
-age=2023-user_yob
-print ("Thanks for your input\n")
-print ("The year of your birth", user_yob)
-print ("Average sleep if you slept 7 hours in night\n")   
-print ("You slept" , 2555 * age, " hours\n")
